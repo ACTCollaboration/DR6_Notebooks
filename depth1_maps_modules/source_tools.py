654079768d0167c0d050fb9a6b85a30b177b1e5d
Allen M. Foster <amfoster@tiger3.princeton.edu>
2025-03-05 18:25:23 -0500

    update act catalog load bc it doesnt have flux

--- depth1_maps_modules/source_tools.py
+++ depth1_maps_modules/source_tools.py
@@ -5,23 +5,18 @@ import numpy as np
 ## requires astropy and photutils
 
 
-def load_act_catalog(source_cat_file:str='PS_S19_f090_2pass_optimalCatalog.fits',
-                    flux_threshold:float=0
+def load_act_catalog(source_cat_file:str='act_sample_cat.fits',
                     ):
     '''
     source_cat_file is path to source catalog
 
-    flux_threshold is a threshold, in Jy, below which we ignore the sources.
-        i.e. if we want sources in depth-1 map, we don't care about sub mJy sources.
-        by default, returns all positive sources.
     '''
     from astropy.table import Table 
     sourcecat=None
     sourcecat = Table.read(source_cat_file)
-    sources = sourcecat[sourcecat["fluxJy"] > (flux_threshold)]
-    sources['RADeg'][sources["RADeg"]<0]+=360.
-    print(len(sources["decDeg"]), 'sources above flux threshold %.1f mJy'%(flux_threshold*1000))
-    return sources
+    sourcecat['RADeg'][sourcecat["RADeg"]<0]+=360.
+    print(len(sourcecat["decDeg"]), 'sources in catalog.')
+    return sourcecat
 
 
 def radec_to_str_name(ra:float, 
